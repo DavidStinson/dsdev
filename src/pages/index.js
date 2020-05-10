f50e8914b8449632570a1f33b42a00b096b64518
David Stinson <mail@davidstinson.dev>
2020-05-10 00:12:23 -0500

    build(page): add hero landing to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Foundation from "../components/foundation";
+import ReactFitText from "react-fittext";
 
 import "./global.css";
 import "./index.css";
 
 export default () => (
   <Foundation>
-    <h1>Hello world!</h1>
+    <section className="hero">
+      <div className="hero-left"></div>
+      <div className="hero-right"></div>
+      <ReactFitText compressor={1} minFontSize={64} maxFontSize={350}>
+        <h1 className="overwhelmingly-large">
+          Hey, <br /> I'm David
+        </h1>
+      </ReactFitText>
+    </section>
+    <p className="arrow bouncy">∨</p>
   </Foundation>
 );
