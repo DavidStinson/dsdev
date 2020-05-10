57e1359438740240354e6fcc6451f20ca8035555
David Stinson <mail@davidstinson.dev>
2020-05-10 17:34:47 -0500

    revert(deps): remove react-fittext

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Foundation from "../components/foundation";
-import ReactFitText from "react-fittext";
 
 import "./global.css";
 import "./index.css";
@@ -10,11 +9,7 @@ export default () => (
     <section className="hero">
       <div className="hero-left"></div>
       <div className="hero-right"></div>
-      <ReactFitText compressor={1} minFontSize={64} maxFontSize={350}>
-        <h1 className="overwhelmingly-large">
-          Hey, <br /> I'm David
-        </h1>
-      </ReactFitText>
+
     </section>
     <p className="arrow bouncy">∨</p>
   </Foundation>
