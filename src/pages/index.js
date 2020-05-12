b661591f1247d06cba6be33c929d3bcc307a151b
David Stinson <mail@davidstinson.dev>
2020-05-11 20:38:58 -0500

    build(page): add hero section to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,14 @@ import "./index.css";
 export default () => (
   <Foundation>
     <main className="main-grid">
-
+      <section className="hero">
+        <div className="hero-text">
+          <h1 className="overwhelmingly-large">Hey, I'm David Stinson</h1>
+          <h2 className="oversized">
+            I build quality software and websites in Austin, TX
+          </h2>
+        </div>
+      </section>
     </main>
   </Foundation>
 );
