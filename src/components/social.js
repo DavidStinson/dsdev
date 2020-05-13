b2f50666eb080c2bef4ac6cd524dad8603c4c7dd
David Stinson <mail@davidstinson.dev>
2020-05-12 19:36:40 -0500

    build(component): add github and linkedin to social component

--- src/components/social.js
+++ src/components/social.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+import twitter from "../assets/images/twitter.png";
+import linkedin from "../assets/images/linkedin.png";
+import github from "../assets/images/github-dark.png";
+
 import "aos/dist/aos.css";
 import "./social.css";
 
@@ -16,14 +20,46 @@ class Social extends Component {
 
   render() {
     return (
-      <div
-        className="social-name-container item item--primary"
-        data-aos="fade-down"
-        data-aos-anchor="#portfolio"
-        data-aos-anchor-placement="top-top"
-      >
-        David Stinson
-      </div>
+      <aside className="social-container">
+        <div
+          className="social-name-container item item--primary"
+          data-aos="fade-down"
+          data-aos-anchor="#portfolio"
+          data-aos-anchor-placement="top-top"
+        >
+          <span id="social-name">David Stinson</span>
+        </div>
+        <div className="social-site-container">
+          <a
+            className="social-link"
+            href="https://linkedin.com/in/david-stinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedin}
+              alt="LinkedIn Logo"
+              className="social-logo"
+            ></img>
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/DavidStinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="GitHub Logo" id="github-logo-small"></img>
+          </a>
+          <a
+            className="social-link"
+            href="https://twitter.com/davidstinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="Twitter Logo" className="social-logo"></img>
+          </a>
+        </div>
+      </aside>
     );
   }
 }
