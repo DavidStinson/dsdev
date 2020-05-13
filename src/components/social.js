6cb1854ed3362295883f4f2d4fb7e030c6967f86
David <49245298+DavidStinson@users.noreply.github.com>
2020-05-12 20:17:02 -0500

    build(component): complete social component (#8)
    * build(component): add foundation component
    
    * style(component): foundation nav styling
    
    * build(component): add site-hero to nav
    
    * build(deps): add gatsby-image
    
    * build(component): finish initial foundation component
    
    * build(page): add hero landing to index page
    
    * chore(style): remove unused font
    
    * revert(deps): remove react-fittext
    
    * refactor(page): rebuild index page
    
    * build(page): add hero section to index
    
    * style(component): cleanup nav grid alignment
    
    * chore(format): code cleanup
    
    * chore(format): code cleanup
    
    * build(component): add github and linkedin to social component
    
    * style(component): complete social component styling

---
+++ src/components/social.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+import twitter from "../assets/images/twitter.png";
+import linkedin from "../assets/images/linkedin.png";
+import github from "../assets/images/github-dark.png";
+
+import "aos/dist/aos.css";
+import "./social.css";
+
+class Social extends Component {
+  componentDidMount() {
+    const AOS = require("aos");
+    this.aos = AOS;
+    this.aos.init({ duration: 900, easing: "ease-in-sine" });
+  }
+
+  componentDidUpdate() {
+    this.aos.refresh();
+  }
+
+  render() {
+    return (
+      <aside
+        className="social-container"
+        data-aos="fade-down"
+        data-aos-anchor="#portfolio"
+        data-aos-anchor-placement="center-top"
+      >
+        <div
+          className="social-name-container item item--primary"
+          id="social-name"
+        >
+          <span>David Stinson</span>
+        </div>
+        <div className="social-site-container">
+          <a
+            className="social-link"
+            href="https://linkedin.com/in/david-stinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedin}
+              alt="LinkedIn Logo"
+              className="social-logo"
+            ></img>
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/DavidStinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="GitHub Logo" id="github-logo-small"></img>
+          </a>
+          <a
+            className="social-link"
+            href="https://twitter.com/davidstinson"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="Twitter Logo" className="social-logo"></img>
+          </a>
+        </div>
+      </aside>
+    );
+  }
+}
+
+export default Social;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,18 @@ export default () => (
     <main className="main-grid">
       <section className="hero">
         <div className="hero-text">
-          <h1 className="overwhelmingly-large">Hey, I'm David Stinson</h1>
-          <h2 className="oversized">
+          <h1 className="overwhelmingly-large-accent">
+            Hey, I'm David Stinson
+          </h1>
+          <h3 className="oversized-subhead">
             I build quality software and websites in Austin, TX
-          </h2>
+          </h3>
         </div>
+      </section>
+      <section className="portfolio">
+        <h2 className="oversized-head" id="portfolio">
+          My Work
+        </h2>
       </section>
     </main>
   </Foundation>
