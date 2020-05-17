9699a2fd83ab7f83697c3ee43b821189523d1755
David Stinson <mail@davidstinson.dev>
2020-05-16 22:38:34 -0500

    chore(format): clean up code

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const isProd = process.env.NODE_ENV === "production"
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   /* Your site config here */
